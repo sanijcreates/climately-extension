f59ae2279360ad3f8ebc52d275ec94e525f0f47b
Marko Tsymbaluk <markotsymbaluk@gmail.com>
2025-02-04 13:40:34 -0500

    New Extension Folder

--- frontend/content.js
+++
@@ -1,9 +0,0 @@
-navigator.geolocation.getCurrentPosition(function (position) {
-  alert(
-    "Latitude : " +
-      position.coords.latitude +
-      ":" +
-      "Longitude : " +
-      position.coords.longitude
-  );
-});
